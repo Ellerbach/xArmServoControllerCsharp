07a2883a32700f7fa6bcfa8b19252ee5512cde12
Laurent Ellerbach <laurelle@microsoft.com>
2023-09-12 15:42:18 -0700

    Adding servo enum, checks and unitsnet

---
+++ xArmServo/Servo.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to Laurent Ellerbach and contributors under one or more agreements.
+// Laurent Ellerbach and contributors license this file to you under the MIT license.
+
+namespace xArmServo
+{
+    /// <summary>
+    /// Servo to control on the xArm.
+    /// </summary>
+    public enum Servo : byte
+    {
+        /// <summary>Servo 1.</summary>
+        S1 = 1,
+
+        /// <summary>Servo 2.</summary>
+        S2 = 2,
+
+        /// <summary>Servo 3.</summary>
+        S3 = 3,
+
+        /// <summary>Servo 4.</summary>
+        S4 = 4,
+
+        /// <summary>Servo 5.</summary>
+        S5 = 5,
+
+        /// <summary>Servo 6.</summary>
+        S6 = 6
+    }
+}
